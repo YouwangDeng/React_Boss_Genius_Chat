500e55404005139133dc3ca178b086506425d0d1
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-05-30 16:41:22 +0800

    add Index.js

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Login from './container/login/login'
+import Register from './container/register/register'
+import AuthRoute from './component/authroute/authroute'
+import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
+import DashBoard from './component/dashboard/dashboard'
+import Chat from './component/chat/chat'
+import reducers from './reducer'
+import './config'
+import './index.css'
+
+const store = createStore(reducers, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f=>f
+))
+ReactDom.render(
+    (<Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <AuthRoute></AuthRoute>
+                <Switch>
+                    <Route path='/bossinfo' component={BossInfo}></Route>
+                    <Route path='/geniusinfo' component={GeniusInfo}></Route>
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/register' component={Register}></Route>
+                    <Route path='/chat/:user' component={Chat}></Route>
+                    <Route component={DashBoard}></Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>), 
+    document.getElementById('root')
+)
